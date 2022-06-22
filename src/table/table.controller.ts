f590c4446ceb544adb02588f874b9eae2b6e58ce
JoaoLeite47 <StalkerJJ@outlook.com>
2022-06-22 17:05:10 -0300

    docs: implement swagger and validation

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/create-table.dto';
 import { TableService } from './table.services';
 
+@ApiTags('table') //swagger tag
 @Controller('table')
 export class TableController {
   constructor(private tableService: TableService) {}
