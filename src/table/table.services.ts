0eca7999444d3f1a1019b2908fcfb4e9bb30f5e8
JoaoLeite47 <StalkerJJ@outlook.com>
2022-06-22 16:37:08 -0300

    feat(table) implement creat table endpoint

--- src/table/table.services.ts
+++ src/table/table.services.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTableDto } from './dto/create-table.dto';
+import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
+  tables: Table[] = [];
+
   create(createTableDto: CreateTableDto) {
-    return 'criar uma mesa! ' + JSON.stringify(createTableDto);
+    const table: Table = { id: 'ramdom_id', ...createTableDto };
+
+    this.tables.push(table);
+
+    return table;
   }
+
   findAll() {
     return 'Buscar todas as mesas';
   }
