061e16bf89739891dcf26c9376b85eb96244de61
JoaoLeite47 <StalkerJJ@outlook.com>
2022-07-02 18:28:33 -0300

    feat<product> implement service/module shema with db

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: string): Promise<Product> {
+    const record = await this.prisma.product.findUnique({ where: { id } });
+    if (!record) {
+      throw new NotFoundException(` ${id} not found!`);
+    }
+    return record;
+  } //method for err tratament
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLines = errorLines[errorLines.length - 1]?.trim(); //pego a mensagem de err da ultima linha
+    throw new UnprocessableEntityException(
+      lastErrorLines || 'Algum erro ocorreu ao criar uma mesa!',
+    );
+  } //throw para erros de tipos na criação de um novo objeto
+
+  async create(dto: CreateProductDto): Promise<Product> {
+    const data: Product = { ...dto };
+
+    return this.prisma.product.create({ data }).catch(this.handleError);
   }
 
-  findAll() {
-    return `This action returns all product`;
+  findAll(): Promise<Product[]> {
+    return this.prisma.product.findMany();
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string): Promise<Product> {
+    return this.findById(id);
   }
 
-  update(id: string, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, dto: UpdateProductDto): Promise<Product> {
+    await this.findById(id);
+    const data: Partial<Product> = { ...dto };
+    return this.prisma.product
+      .update({ where: { id }, data })
+      .catch(this.handleError);
   }
 
-  delete(id: string) {
-    return `This action removes a #${id} product`;
+  async delete(id: string) {
+    await this.findById(id);
+    await this.prisma.product.delete({ where: { id } });
   }
 }
--- src/table/dto/update-table.dto.ts
+++ src/table/dto/update-table.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateTableDto } from './create-table.dto';
 
-export class UpdateTableDto extends PartialType(CreateTableDto) {} //dto simplificado para funcionar com variaveis opcionais, utilizando a lib PartialType, onde não mais o PUT é feito e sim um Patch
+export class UpdateTableDto extends PartialType(CreateTableDto) {}
