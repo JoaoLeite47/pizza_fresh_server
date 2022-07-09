783d4f43c3ec5396c9bbf6d7743549a0c72c9887
JoaoLeite47 <StalkerJJ@outlook.com>
2022-07-09 18:17:12 -0300

    feat<user> dto and entity created

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({ description: 'Nome do usuário', example: 'Jao' })
+  nickname: string;
+
+  @ApiProperty({
+    description: 'Nome do usuário de perfil',
+    example: 'João Leite',
+  })
+  name: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({ description: 'Senha do usuário', example: 'new_password' })
+  password: string;
+
+  @ApiProperty({
+    description: 'As senhas devem ser identicas!',
+    example: 'new_password',
+  })
+  ConfirmPassword: string;
+
+  @IsUrl()
+  @ApiProperty({ description: 'URL da imagem do usuário', example: 'http://' })
+  image: string;
+}
