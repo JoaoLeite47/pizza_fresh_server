1a4d3243f7c2af40570a0652fca783a5cbb2e5f3
JoaoLeite47 <StalkerJJ@outlook.com>
2022-07-18 16:42:05 -0300

    feat<documentation> implement generate campodocs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,12 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  /**
+   * Recebe uma requisição GET e retorna um objeto de status
+   * da aplicação com a URL de documentação
+   * @param req Objeto de Request do Express
+   * @returns Objeto de status da aplicação
+   */
   @Get()
   getAppStatus(@Req() req: Request) {
     const baseURL = req.protocol + '://' + req.get('host'); //Base URl montado de acordo com a url do deploy
